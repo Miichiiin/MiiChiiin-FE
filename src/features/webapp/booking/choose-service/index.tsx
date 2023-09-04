66f53493a863848c154a76f887036aa5caacc80d
chuoi2taps <thiennnph25438@fpt.edu.vn>
2023-09-04 16:35:30 +0700

    HM-104: Choose Services - Checkbox services - Total price

--- src/features/webapp/booking/choose-service/index.tsx
+++ src/features/webapp/booking/choose-service/index.tsx
@@ -1,9 +1,28 @@
-// import { GrLocation } from 'react-icons/gr'
-// import { HiOutlineUser } from 'react-icons/hi'
-import { AiOutlineExpandAlt, AiOutlineInfoCircle, AiFillCheckCircle, AiOutlineCheckCircle, AiOutlineDown, AiOutlineUp, AiOutlineArrowRight } from 'react-icons/ai'
-
+import { useGetService_hotelQuery } from '@/api/service_hotel';
+import { useState } from 'react';
+import { AiOutlineInfoCircle, AiFillCheckCircle, AiOutlineCheckCircle, AiOutlineDown, AiOutlineUp, AiOutlineArrowRight } from 'react-icons/ai'
 import { Link } from 'react-router-dom'
 const ChooseService = () => {
+    const toggleShowService = () => {
+        setIsServiceOpen(!isServiceOpen);
+    };
+    const [isServiceOpen, setIsServiceOpen] = useState(false);
+    const [selectedServices, setSelectedServices] = useState<any[]>([]);
+    const [totalPrice, setTotalPrice] = useState(0); // Tổng tiền
+    // Hàm để thêm hoặc bỏ chọn dịch vụ
+    const toggleServiceSelection = (serviceId: number, servicePrice:number) => {
+        if (selectedServices.includes(serviceId)) {
+            // Nếu dịch vụ đã được chọn, loại bỏ nó khỏi danh sách
+            setSelectedServices(selectedServices.filter((id) => id !== serviceId));
+            setTotalPrice(totalPrice - servicePrice);
+        } else {
+            // Nếu dịch vụ chưa được chọn, thêm nó vào danh sách
+            setSelectedServices([...selectedServices, serviceId]);
+            setTotalPrice(totalPrice + servicePrice);
+        }
+    };
+    const { data: serviceData, isLoading } = useGetService_hotelQuery({})
+
     return (
         <div className='max-w-7xl mx-auto '>
             {/*Content*/}
@@ -30,24 +49,35 @@ const ChooseService = () => {
                                     <h1 className='font-semibold text-xl'>Phòng 1</h1>
                                     <div className='flex justify-between'>
                                         <h1 className=''>Biệt thự 1 phòng ngủ</h1>
-                                        <button className='text-yellow-700 hover:text-yellow-500 font-semibold flex items-center'>Ẩn dịch vụ <AiOutlineUp /></button>
+                                        <button
+                                            onClick={toggleShowService}
+                                            className='text-yellow-700 hover:text-yellow-500 font-semibold '
+                                        >
+                                            {isServiceOpen ? <span className='flex items-center'>Ẩn dịch vụ <AiOutlineUp /></span> : <span className='flex items-center'>Hiện dịch vụ <AiOutlineDown /></span>}
+                                        </button>
                                     </div>
                                 </div>
                                 <hr className='my-2' />
-                                <div className='grid grid-cols-3 gap-2 pt-2'>
-                                    <div className="column border">
-                                        <img src="https://booking-static.vinpearl.com/hotels/amenity/9d1b2f4b552c43418d3c83a4525512a6_VAP%20PQ.jpg" alt="" className='w-full' />
-                                        <h1 className='pl-2 pt-2 pb-5'>VinWonders & Vinpearl Safari Phú Quốc</h1>
-                                        <p className='pl-2 pb-2 font-semibold text-yellow-800 text-lg'>9.400.000đ</p>
-                                        <div className='flex justify-between items-center px-2 py-3'>
-                                            <button className='flex items-center hover:text-yellow-500'><AiOutlineInfoCircle /><span className='pl-1'>Chi tiết</span></button>
-                                            <label className='items-center flex'>
-                                                <input type="checkbox" className="w-5 h-5 text-yellow-600 bg-yellow-500 border-yellow-500 rounded checked:bg-yellow-500 " />
-                                                <span className="ml-2 text-sm font-medium text-yellow-800">Thêm</span>
-                                            </label>
-                                        </div>
+                                {/*Bật tắt hiện dịch vụ*/}
+                                {isServiceOpen && (
+                                    <div className='grid grid-cols-3 gap-2 pt-2'>
+                                        {serviceData?.map((item: any) => (
+                                            <div className="column border" key={item.id}>
+                                                <img src={item?.image} className='w-full' />
+                                                <h1 className='pl-2 pt-2 pb-5'>{item.name}</h1>
+                                                <p className='pl-2 pb-2 font-semibold text-yellow-800 text-lg'>{item.price}<span className='text-sm'> vnđ</span></p>
+                                                <div className='flex justify-between items-center px-2 py-3'>
+                                                    <button className='flex items-center hover:text-yellow-500'><AiOutlineInfoCircle /><span className='pl-1'>Chi tiết</span></button>
+                                                    <label className='items-center flex' >
+                                                        <input type="checkbox" className="w-5 h-5 text-yellow-600 bg-yellow-500 border-yellow-500 rounded checked:bg-yellow-500 " onChange={() => toggleServiceSelection(item.id, item.price)}
+                                                            checked={selectedServices.includes(item.id)} />
+                                                        <span className="ml-2 text-sm font-medium text-yellow-800">Thêm</span>
+                                                    </label>
+                                                </div>
+                                            </div>
+                                        ))}
                                     </div>
-                                </div>  
+                                )}
                             </section>
                         </div>
                     </div>
@@ -81,16 +111,28 @@ const ChooseService = () => {
                                 </div>
                                 <p className='text-sm pt-3 items-center flex'><span className='pr-1'>x1</span> Biệt Thự 2 Phòng Ngủ</p>
                                 <p className='text-sm pb-3'>2 Người lớn, 2 Trẻ em</p>
-                                {/*Dịch vụ đã đặt*/}
+                                {/*Dịch vụ đã chọn*/}
                                 <div className='border-gray-100 bg-gray-100 px-2 rounded'>
                                     <p className='text-sm pb-3 font-semibold'>Dịch vụ mua thêm</p>
+
                                     <ul className='list-disc px-3'>
-                                        <li className='text-sm pb-2'>
-                                            <div className='flex justify-between items-center'>
-                                                <p>VinWonders & Vinpearl Safari Phú Quốc <span>x4</span></p> <p>9.400.000đ</p>
-                                            </div>
-                                        </li>
+                                        {selectedServices.map((selectedServiceId) => {
+                                            const selectedService = serviceData.find((item:any) => item.id === selectedServiceId);
+                                            if (selectedService) {
+                                                return (
+                                                    <li className='text-sm pb-2' key={selectedService.id}>
+                                                        <div className='flex justify-between items-center'>
+                                                            <p>{selectedService.name}</p>
+                                                            <p>{selectedService.price} vnđ</p>
+                                                        </div>
+                                                    </li>
+                                                );
+                                            }
+                                            return null;
+                                        })}
                                     </ul>
+
+
                                 </div>
                             </div>
                             <hr className='my-4' />
@@ -100,7 +142,7 @@ const ChooseService = () => {
                                     <h1 className='font-semibold'>
                                         Tổng cộng:
                                     </h1>
-                                    <h1 className='text-xl font-bold text-yellow-500'>38.920.000</h1>
+                                    <h1 className='text-xl font-bold text-yellow-500'>{totalPrice} vnđ</h1>
                                 </div>
                             </div>
                             <button className='bg-yellow-500 hover:bg-yellow-600 text-white py-3 px-2 text-lg font-bold rounded-full w-full'>Tiếp tục</button>
